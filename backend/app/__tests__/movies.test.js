bf86375687b429d4a5cf7b64c828684495c7c685
Brighton Balfrey <balfrey@usc.edu>
2019-09-28 14:29:36 -0700

    Added link in docker compose. Fixed Dockerfiles. Added base64 images reponse json in backend. Added endpoint to get images. Added unit tests for new endpoint

--- backend/app/__tests__/movies.test.js
+++ backend/app/__tests__/movies.test.js
@@ -2,16 +2,28 @@ const app = require('../app.js');
 const supertest = require('supertest')
 const request = supertest(app);
 const sample_data = require('../../sample_data/data.json');
+const sample_images = require('../../sample_data/images.json');
 
-test('Endpoint is accessible', async (done) => {
+test('Movies endpoint is accessible', async (done) => {
 	const res = await request.get('/movies');
 	expect(res.status).toBe(200);
 	done();
 });
 
-test('Endpoint has data', async (done) => {
+test('Movies endpoint has data', async (done) => {
 	const res = await request.get('/movies');
 	expect(JSON.parse(res.body)).toStrictEqual(sample_data);
 	done();
 });
 
+test('Images endpoint is accessible', async (done) => {
+	const res = await request.get('/movies/images');
+	expect(res.status).toBe(200);
+	done();
+});
+
+test('Images endpoint has data', async (done) => {
+	const res = await request.get('/movies/images');
+	expect(JSON.parse(res.body)).toStrictEqual(sample_images);
+	done();
+});
--- backend/app/routes/movies.js
+++ backend/app/routes/movies.js
@@ -2,10 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 let sample_data = require('../../sample_data/data.json');
+let sample_images = require('../../sample_data/images.json');
 
-/* GET users listing. */
+/* GET movie info */
 router.get('/', function(req, res, next) {
 	res.json(JSON.stringify(sample_data));
+	res.send();
+});
+
+/* GET movie images */
+router.get('/images', function(req, res, next) {
+	res.json(JSON.stringify(sample_images));
 	res.send();
 });
 
